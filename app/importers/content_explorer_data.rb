bedce4a5e04e2d59a845153369021275c5ba720e
James Isaacs <james@govwizely.com>
2015-02-14 15:09:02 -0800

    Code reformatting.

--- app/importers/content_explorer_data.rb
+++ app/importers/content_explorer_data.rb
@@ -4,7 +4,6 @@ class ContentExplorerData
   include Importer
 
   def import
-    # currently pulling from localhost
     mongo_client = Mongo::MongoClient.new('localhost', 27017)
     db = mongo_client.db("test")
     coll = db["infoSave"]
@@ -16,23 +15,22 @@ class ContentExplorerData
   private
 
   def process_article_info(article_hash)
-    article = {}
-    article[:mongo_id] = article_hash["_id"].to_s
-    article[:trade_topics] = article_hash["TradeTopics"]
-    article[:region] = article_hash["Region"]
-    article[:sub_region] = article_hash["SubRegion"]
-    article[:country] = article_hash["Country"]
-    article[:industry] = article_hash["Industry"]
-    article[:sector] = article_hash["Sector"]
-    article[:sub_sector] = article_hash["SubSector"]
-    article[:document_type] = article_hash["DocumentType"]
-    article[:content] = Sanitize.clean(article_hash["Content"])
+    article                  = {}
+    article[:mongo_id]       = article_hash["_id"].to_s
+    article[:trade_topics]   = article_hash["TradeTopics"]
+    article[:region]         = article_hash["Region"]
+    article[:sub_region]     = article_hash["SubRegion"]
+    article[:country]        = article_hash["Country"]
+    article[:industry]       = article_hash["Industry"]
+    article[:sector]         = article_hash["Sector"]
+    article[:sub_sector]     = article_hash["SubSector"]
+    article[:document_type]  = article_hash["DocumentType"]
+    article[:content]        = Sanitize.clean(article_hash["Content"])
     article[:version_number] = article_hash["VersionNumber"]
-    article[:created_date] = Date.parse(article_hash["CreatedDate"].to_s)
-    article[:updated_date] = Date.parse(article_hash["UpdatedDate"].to_s)
-    article[:created_by] = article_hash["CreatedBy"]
-    article[:updated_by] = article_hash["UpdatedBy"]
+    article[:created_date]   = Date.parse(article_hash["CreatedDate"].to_s)
+    article[:updated_date]   = Date.parse(article_hash["UpdatedDate"].to_s)
+    article[:created_by]     = article_hash["CreatedBy"]
+    article[:updated_by]     = article_hash["UpdatedBy"]
     article
   end
 end
-
